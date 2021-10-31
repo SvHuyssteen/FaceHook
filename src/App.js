1be4a8e87ade8f4299a157e5d238eac7ce35b974
Shaun van Huyssteen <littleblueline@gmail.com>
2021-10-31 15:28:03 +0200

    initial deployment

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
 
   OnButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://haunted-shadow-20574.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -78,7 +78,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://haunted-shadow-20574.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
